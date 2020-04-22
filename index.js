969ffccd3408dd16f6208266a2109d730db47098
sophiasagan <sophiabraddock82@gmail.com>
2020-04-22 11:45:51 -0400

    mvp

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const server = require("./api/server.js");
+//const server = require("./api/server.js");
+const express = require("express")
+const welcomeRouter = require("./welcome/welcome-router")
+const accountsRouter = require("./budget/accounts-router")
 
+const server = express()
 const PORT = process.env.PORT || 5000;
+
+server.use(express.json())
+server.use("/", welcomeRouter)
+server.use("/accounts", accountsRouter)
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
 
 server.listen(PORT, () => {
   console.log(`\n== API running on port ${PORT} ==\n`);
